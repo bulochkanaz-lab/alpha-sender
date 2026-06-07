ccf01d072178224f23163e767c4baa529bd46501
Andriy <bulochka.naz@gmail.com>
2026-06-07 13:35:37 +0300

    Оновив дизайн вінків

--- payload-fs.js
+++ payload-fs.js
@@ -1062,7 +1062,7 @@ function injectBotUI() {
 
                 <div class="alpha-nav">
                     <div id="tabBtnSettings" data-lang="tabSettings" class="alpha-nav-btn active">🚀 Розсилка</div>
-                    <div id="tabBtnInvites" data-lang="tabInvites" class="alpha-nav-btn" style="display: none;">📩 Інвайти</div>
+                    <div id="tabBtnInvites" data-lang="tabInvites" class="alpha-nav-btn" style="display: none;">📩 Інвайтики</div>
                     <div id="tabBtnLetters" data-lang="tabLetters" class="alpha-nav-btn" style="display: none;">📝 Листи</div>
                     <div id="tabBtnWinks" data-lang="tabWinks" class="alpha-nav-btn">😉 Вінки/Лайки</div>
                     <div id="tabBtnVip" data-lang="tabVip" class="alpha-nav-btn">🚨 VIP Радар</div>
